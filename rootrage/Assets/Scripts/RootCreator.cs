665dde1d0a89930a4c0ba7038e14f44dfcb5cbd5
Holger <hxpb@gmx.de>
2023-02-04 12:46:44 +0100

    ﻿ADDED: Layer/Tags for marking Player1/2 and Obstacles CHANGED: Roots will now end, if they hit an obstacle/player OR if you release the button

--- rootrage/Assets/Scripts/RootCreator.cs
+++ rootrage/Assets/Scripts/RootCreator.cs
@@ -1,27 +1,53 @@
-using System;
 using System.Threading.Tasks;
-using UnityEditor;
 using UnityEngine;
-using Quaternion = System.Numerics.Quaternion;
-using Random = UnityEngine.Random;
+using UnityEngine.InputSystem;
 
 public class RootCreator : MonoBehaviour
 {
     [SerializeField] private GameObject rootPrefab;
-    [SerializeField] private int delayRootParts = 200;
+    [SerializeField] private int delayRootParts = 100;
+    [SerializeField] private int maxLength = 16;
+    [SerializeField] private LayerMask layermask = 1 << 8 | 1 << 7 | 1 << 6; //by default 'player' and 'obstacle'
+    
+    [SerializeField]
+    private InputActionReference jumpAction;
+    
+    //Todo: particle in front, mesh in front different from rest, sound...
 
     public async void ShootForward()
     {
-        var rootParent = new GameObject();
+        var rootParent = new GameObject("RootShoot");
         var startRot = transform.rotation;
         var startForward = transform.forward;
-        
         var currentRootPos = transform.position + startForward;
-        while (currentRootPos.x < 16 && currentRootPos.x > -16 && currentRootPos.z < 16 && currentRootPos.z > -16)
+        int currentLength = 0;
+        
+        while (currentLength < maxLength)
         {
+            currentLength++;
+            if (!jumpAction.action.IsPressed())
+            {
+                //stop rooting, when button not pressed anymore.
+                break;
+            }
+                
+            var hitDetect = Physics.BoxCast(currentRootPos, Vector3.one * 0.5f, startForward, out RaycastHit hit, startRot, 1, layermask);
             Instantiate(rootPrefab, currentRootPos, startRot, rootParent.transform);
-            await Task.Delay(delayRootParts);
             currentRootPos += startForward;
+            if (hitDetect)
+            {
+                //something was hit, end the root here
+                if (hit.transform.gameObject.CompareTag("Player"))
+                {
+                    //TODO: inform player about hit
+                    Debug.Log("A Player was hit");
+                }
+                break;
+            }
+            else
+            {
+                await Task.Delay(delayRootParts);
+            }
         }        
     }
 }
