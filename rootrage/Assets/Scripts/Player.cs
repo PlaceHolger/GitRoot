2de2bd49ec19160beab8b113b86519b2923a552d
Holger <hxpb@gmx.de>
2023-02-04 19:40:44 +0100

    ADDED: Player(hit) script now also triggers hit anim

--- rootrage/Assets/Scripts/Player.cs
+++ rootrage/Assets/Scripts/Player.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private GameObject HitParticle;
+    [SerializeField] private Animator animator;
     [SerializeField] private float HitFeedbackDuration = 0.5f;
     
     public void OnHit()
     {
+        if (animator)
+        {
+            animator.ResetTrigger("Attack");
+            animator.ResetTrigger("Shoot");
+            animator.SetBool("isShooting", false);
+            animator.SetTrigger("Hit");
+        }
         var hitParticle = Instantiate(HitParticle, transform);
         Destroy(hitParticle.gameObject, HitFeedbackDuration);
     }
