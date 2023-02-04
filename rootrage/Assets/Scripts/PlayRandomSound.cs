44e1151f0d99071bb66b7bf0fb914dc787484bc7
Holger <hxpb@gmx.de>
2023-02-04 21:55:17 +0100

    ADDED: Simple helper to play a random audioClip from a list of clips

---
+++ rootrage/Assets/Scripts/PlayRandomSound.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayRandomSound : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSourceToUse;
+    [SerializeField] private List<AudioClip> clips;
+
+    private void Awake()
+    {
+        if (audioSourceToUse)
+            audioSourceToUse = GetComponent<AudioSource>();
+    }
+
+    [ContextMenu("Play Random Sound")]
+    public void Play()
+    {
+        int randomId = Random.Range(0, clips.Count);
+        audioSourceToUse.PlayOneShot(clips[randomId]);
+    }
+}
